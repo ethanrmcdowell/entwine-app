12fbe850a96a3f2359333caf063b8e55092eb504
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2023-10-18 11:28:28 -0400

    Firebase setup

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'entwine-app-80aa4',
+    appId: '1:936252877664:web:504ce2a2265b3a96fa8c05',
+    storageBucket: 'entwine-app-80aa4.appspot.com',
+    apiKey: 'AIzaSyCK2c9eWu-xIDPZDBGpVYmfOMTh2ofgCew',
+    authDomain: 'entwine-app-80aa4.firebaseapp.com',
+    messagingSenderId: '936252877664',
+    measurementId: 'G-7LFK4HSY46',
+  },
+};
