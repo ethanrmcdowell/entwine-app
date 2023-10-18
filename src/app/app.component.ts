6a06ffcce72d82390236910b44291acc0e29f7ad
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2023-10-18 12:56:48 -0400

    playing around with styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('menuAnimation', [
+      state('closed', style({ height: '0px' })),
+      state('open', style({ height: '200px' })),
+      transition('closed => open', animate('250ms ease-in-out')),
+      transition('open => closed', animate('250ms ease-in-out'))
+    ])
+  ]
 })
 export class AppComponent {
   title = 'entwine-app';
+  menuState = 'closed';
+
+  toggleMenu() {
+    this.menuState = this.menuState === 'closed' ? 'open' : 'closed';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,29 @@ import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAuth(() => getAuth()),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
+    BrowserAnimationsModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
